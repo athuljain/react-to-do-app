63ef7426540f5787b26349fd7c1f475398d4675c
athuljain <athuljainkj2@gmail.com>
2023-09-26 15:00:52 +0530

    styile commit

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -1,95 +1,93 @@
-import { useState } from "react"
+import { useState } from "react";
+import "./Todo.css"
 
-export default function Todo(){
+export default function Todo() {
+  const [task, setTask] = useState([]);
+  const [inputTask, setInputTask] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
 
-    const [task,setTask]=useState([])
-    const [inputTask,setInputTask]=useState("")
-
-
-    const [editIndex, setEditIndex] = useState(-1);
-
-
-
-function AddTask(){
-
-    if(inputTask.trim() !== ""){  //check the input filed if any value is in input filed
-        setTask([...task,inputTask])
-        setInputTask("") // clear input after adding task
-
+  function AddTask() {
+    if (inputTask.trim() !== "") {
+      //check the input filed if any value is in input filed
+      setTask([...task, inputTask]);
+      setInputTask(""); // clear input after adding task
     }
+  }
 
 
 
-   
-}
 
-function DeleteTask(index){
-    const updatedTaskList=[...task]
-    updatedTaskList.splice(index,1) // delete the specific task
-    setTask(updatedTaskList)
-}
-    
-
-    return(
-        <div>
-            <h1>React To-Do-app</h1>
-<div>
-            <input type="text" placeholder="add task" value={inputTask} onChange={((e)=> setInputTask( e.target.value))}></input>
-            <button onClick={AddTask} >Add Task</button>
-
-            </div>
-
-<div>
+  function DeleteTask(index) {
+    const updatedTaskList = [...task];
+    updatedTaskList.splice(index, 1); // delete the specific task
+    setTask(updatedTaskList);
+  }
 
 
 
-    <ul>
+  return (
+    <div className="main-container">
+      <h1 className="heading">React To-Do-app</h1>
+      <div className="input-div">
+        <input
+          className="main-input"
+          type="text"
+          placeholder="add task"
+          value={inputTask}
+          onChange={(e) => setInputTask(e.target.value)}
+        ></input>
+        <button className="add-button" onClick={AddTask}>
+          Add Task
+        </button>
+      </div>
 
-
-    {task.map((data, index) => (
-    <li key={index}>
-        {editIndex === index ? (
-            <div>
-                <input
+      <div className="ul-div">
+        <ul className="ul-tag">
+          {task.map((data, index) => (
+            <li className="li-tag" key={index}>
+              {editIndex === index ? (
+                <div className="li-div">
+                  <input
+                    className="editInput"
                     type="text"
                     value={data}
                     onChange={(e) => {
-                        const updatedTaskList = [...task];
-                        updatedTaskList[index] = e.target.value;
-                        setTask(updatedTaskList);
+                      const updatedTaskList = [...task];
+                      updatedTaskList[index] = e.target.value;
+                      setTask(updatedTaskList);
                     }}
-                />
-                <button
+                  />
+                  <button
+                    className="saveBtn"
                     onClick={() => {
-                        setEditIndex(-1); // Exit edit mode
+                      setEditIndex(-1); // Exit edit mode
                     }}
-                >
+                  >
                     Save
-                </button>
-            </div>
-        ) : (
-            <div>
-                {data}
-                <button
+                  </button>
+                </div>
+              ) : (
+                <div className="edit-div">
+                  {data}
+                  <button
+                    className="editBtn"
                     onClick={() => {
-                        setEditIndex(index); // Enter edit mode
-                    }}
-                >
+                      setEditIndex(index); // Enter edit mode
+                    }}>
                     Edit
-                </button>
+                  </button>
 
-                    <button onClick={()=>DeleteTask(index)}>Delete</button>
-
-            </div>
-        )}
-    </li>
-))}
-    </ul>
-</div>
-
-
-
-
-        </div>
-    )
+                  <button
+                    className="DeleteBtn"
+                    onClick={() => DeleteTask(index)}>
+                    Delete
+                  </button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
