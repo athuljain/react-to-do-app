b2b930c10192f994051ae1f286b196bb99dd3b67
athuljain <athuljainkj2@gmail.com>
2023-09-27 08:05:17 +0530

    home page commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Component/Home';
 import Todo from './Component/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Todo />
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/todo' element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
+        
     </div>
   );
 }
