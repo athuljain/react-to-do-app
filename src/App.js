3b648512af927d148c2119a1597e9076b754dda0
athuljain <athuljainkj2@gmail.com>
2023-09-30 12:23:49 +0530

    home page commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/todo' element={<Todo />} />
+          
         </Routes>
       </BrowserRouter>
         
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,7 +1,14 @@
-export default function Home(){
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+  function TodoBtn() {
+    navigate("/todo");
+  }
+  return (
+    <div className="HomePageContainer">
+      <h1 className="H1HomePage">Welocome !!!</h1>
+      <button className="ToDoBtn" onClick={TodoBtn}>To-Do-App</button>
+    </div>
+  );
 }
