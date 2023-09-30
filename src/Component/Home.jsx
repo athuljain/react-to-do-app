eff99a48e747c8847f2d75f932eb755ca2128014
athuljain <athuljainkj2@gmail.com>
2023-09-30 13:13:05 +0530

    home page css commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import "./Home.css"
+
 
 export default function Home() {
-  const navigate = useNavigate();
-  function TodoBtn() {
-    navigate("/todo");
-  }
+    const navigate = useNavigate();
+    function TodoBtn() {
+      navigate("/todo");
+    }
+  
+
   return (
-    <div className="HomePageContainer">
-      <h1 className="H1HomePage">Welocome !!!</h1>
+    <div className="HomePageContainer" >
+      <h1 className="H1HomePage">Welcome !!!</h1>
       <button className="ToDoBtn" onClick={TodoBtn}>To-Do-App</button>
     </div>
   );
-}
+    }
